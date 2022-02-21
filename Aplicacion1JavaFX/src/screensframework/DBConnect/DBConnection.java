ef5ff6a55c7417e608d37d82d93fa604a6860965
AmosRodriguez <20197894@itla.edu.do>
2022-02-21 16:02:31 -0300

    First commit

---
+++ Aplicacion1JavaFX/src/screensframework/DBConnect/DBConnection.java
@@ -0,0 +1,41 @@
+package screensframework.DBConnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    
+    private static Connection conn;
+    private static String url = "jdbc:postgresql://localhost:5432/ventas"; //tu cambiaste eso o vino asi? cambien el nombre de ventas a Ventas
+    private static String user = "postgres";
+    private static String pass = "123";// no veo nada relacionado a wilpolanco entonces es la contrase;a
+
+
+    /*
+    private static String url = "jdbc:mysql://localhost/sysventas";
+    private static String user = "root";
+    private static String pass = "wilpolanco";*/
+    
+    public static Connection connect() throws SQLException{
+	try {
+            Class.forName("org.postgresql.Driver").newInstance();
+            //Class.forName("com.mysql.jdbc.Driver").newInstance();
+	} catch(ClassNotFoundException cnfe) {
+            System.err.println("Error: "+cnfe.getMessage());
+	} catch(InstantiationException ie) {
+            System.err.println("Error: "+ie.getMessage());
+	} catch(IllegalAccessException iae) {
+            System.err.println("Error: "+iae.getMessage());
+	}
+            conn = DriverManager.getConnection(url,user,pass);
+            return conn;
+	}
+	
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+        if(conn !=null && !conn.isClosed())
+            return conn;
+            connect();
+            return conn;
+    }
+}
